ff8482fe0a19d54de6ab34fed054cafeb4f72870
andrecarrsd2008 <andrecarrsd2008@gmail.com>
2018-12-07 01:30:51 -0800

    Made final corrections through github

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,9 +1,7 @@
 const axios = require('axios');
 
 exports.handler = function(event, context, callback){
-  const API_URL = 'https://api.github.com/users';
-  const API_CLIENT_ID = '2ba2bcc85fcea544648c';
-  const API_CLIENT_SECRET = 'c906cb6c446fc3c5cd71742ce88f66d894b47a82'
+  const { API_URL, API_CLIENT_ID, API_CLIENT_SECRET } = process.env;
  
   const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
 
